d9562d2920151e7e093b7c88a2e50b4c37d6d0ec
Takeru Sato <type.in.type@gmail.com>
2026-09-10 18:19:38 +0900

    fix: brew path

--- config/fish/conf.d/brew.fish
+++ config/fish/conf.d/brew.fish
@@ -1,7 +1,7 @@
 if test -x /opt/homebrew/bin/brew
     eval "$(/opt/homebrew/bin/brew shellenv fish)"
-else if test -x "$HOME/.linuxbrew/bin/brew"
-    eval "$("$HOME/.linuxbrew/bin/brew" shellenv fish)"
+else if test -x "$HOME/.homebrew/bin/brew"
+    eval "$("$HOME/.homebrew/bin/brew" shellenv fish)"
 else if test -x /home/linuxbrew/.linuxbrew/bin/brew
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
 end

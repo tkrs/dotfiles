429747739ee9204c10b65196987866b4ac34d8be
Takeru Sato <type.in.type@gmail.com>
2026-09-11 10:27:49 +0900

    fix: setup_nvim.sh

--- scripts/setup_nvim.sh
+++ scripts/setup_nvim.sh
@@ -3,10 +3,10 @@
 set -euo pipefail
 
 if command -v nvim >/dev/null 2>&1; then
-  mapfile -t source_files < <(find "$DOTFILES_DIR/nvim" -name "*.lua")
+  mapfile -t source_files < <(find "$DOTFILES_DIR/config/nvim" -name "*.lua")
 
   for source_file in "${source_files[@]}"; do
-    local dest_file="${source_file//$DOTFILES_DIR/$HOME\/.config}"
+    dest_file="${source_file//$DOTFILES_DIR\/config/$HOME\/.config}"
     mkdir -p "$(dirname "$dest_file")"
     ln -sfn "$source_file" "$dest_file"
   done

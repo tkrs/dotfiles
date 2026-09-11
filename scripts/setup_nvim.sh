46f4e32039b755cc37693ac327e8785db7246622
Takeru Sato <type.in.type@gmail.com>
2026-09-11 19:02:28 +0900

    remove unnecessary escape

--- scripts/setup_nvim.sh
+++ scripts/setup_nvim.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 if command -v nvim >/dev/null 2>&1; then
   find "$DOTFILES_DIR/config/nvim" -name '*.lua' | while IFS= read -r source_file; do
-    dest_file="${source_file//$DOTFILES_DIR\/config/$HOME\/.config}"
+    dest_file="${source_file//$DOTFILES_DIR\/config/$HOME/.config}"
     mkdir -p "$(dirname "$dest_file")"
     ln -sfn "$source_file" "$dest_file"
   done
